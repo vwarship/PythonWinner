8eda3bc9b400b988fc2827437aceaea73be39d4e
Junjian Wang <vwarship@163.com>
2017-07-14 11:19:28 +0800

    查询 排序

--- tutorial/mongoengine_.py
+++ tutorial/mongoengine_.py
@@ -7,6 +7,7 @@ User Guide: http://docs.mongoengine.org/guide/index.html
 
 from mongoengine import *
 from datetime import datetime
+from datetime import date
 
 
 # 连接数据库
@@ -43,17 +44,62 @@ page.save()
 
 
 """查询"""
-for page in WikiPage.objects():
-    print(page.title, page.create_date)
+# for page in WikiPage.objects():
+#     print(page.title, page.create_date)
 
 
 """
 choices 指定一个属性的取值
+default 设置默认值
 """
 SEX = ('boy', 'girl')
 class User(Document):
     name = StringField(required=True)
     age = IntField()
     sex = StringField(choices=SEX)
+    birthday = DateTimeField(default=datetime.now())
 
-User(name='wjj', age=36, sex='girl').save()
+
+"""移除User所有文档"""
+User.drop_collection()
+
+
+"""创建多个User文档"""
+users = [('wjj', 36, 'boy', date(1981, 1, 1)),
+         ('wrj', 8, 'boy', date(2009, 1, 1)),
+         ('ns', 20, 'girl', date(1998, 1, 1))]
+for user in users:
+    User(*user).save()
+
+
+"""取文档个数"""
+count = User.objects().count()
+print(count)    # 3
+
+
+"""distinct"""
+sexs = User.objects().distinct(field='sex')
+print(sexs) # ['boy', 'girl']
+
+
+"""查询"""
+User.objects(name='wjj')    # =
+User.objects(age__gt=20)    # >
+User.objects(age__gte=20)   # >=
+User.objects(age__lt=20)    # <
+User.objects(age__lte=20)   # <=
+User.objects(age__ne=20)    # !=
+User.objects(birthday__gte=date(1998, 1, 1))    # >= date
+User.objects(sex__in=['boy', 'girl'])   # in
+User.objects(sex__nin=['girl']) # nin
+
+
+"""排序"""
+User.objects().order_by("age")  # 升序排列
+User.objects().order_by("-age") # 降序排列
+
+
+"""limit skip"""
+User.objects().limit(1)
+User.objects().skip(1).limit(1)
+
