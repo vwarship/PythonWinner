4799fa6ee43f19b2bc486107af9a35c9787baedb
Junjian Wang <vwarship@163.com>
2017-07-15 01:52:20 +0800

    pyqt5

---
+++ tutorial/pyqt5.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5.QtWidgets import QMessageBox, QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hello")
+
+        self.edit_hello = QTextEdit("Hello World!")
+        self.btn_press = QPushButton("Press me")
+
+        self.btn_press.clicked.connect(self.btn_press_clicked)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit_hello)
+        layout.addWidget(self.btn_press)
+        self.setLayout(layout)
+        self.show()
+
+    def btn_press_clicked(self):
+        QMessageBox.information(self, "HI", "Press me!")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    sys.exit(app.exec_())
