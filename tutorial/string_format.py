2c96b87353133cc61ea7a3528889e40e6ef84ad6
Junjian Wang <vwarship@163.com>
2017-07-17 19:13:23 +0800

    string format

---
+++ tutorial/string_format.py
@@ -0,0 +1,7 @@
+"""指定宽度，不足前面补足0"""
+str(1).zfill(5) # 00001
+
+
+"""/home/wjj/test.txt"""
+'{dir}/{filename}.txt'.format(filename='test', dir='/home/wjj')
+'{1}/{0}.text'.format('test', '/home/wjj')
