abed96263628fc263c6ea7846415013e67bb5f4b
Junjian Wang <vwarship@163.com>
2017-07-21 10:02:20 +0800

    取出所有的单词
    生成字符串的简略表示形式（不会超过30个字符）

--- tutorial/regularexps.py
+++ tutorial/regularexps.py
@@ -2,6 +2,7 @@
 正则表达式
 """
 import re
+import reprlib
 
 
 """移除所有html标签"""
@@ -11,3 +12,14 @@ text = re.compile(r'<[^>]+>').sub('', html) # HI Hello World
 
 """利用所有非字母字符拆分出单词"""
 words = re.compile(r'[^A-Z^a-z]+').split(text)  # ['HI', 'Hello', 'World']
+
+
+"""取出所有的单词"""
+text = 'If keys, values and items views are iterated over with no intervening modifications to the dictionary, the order of items will directly correspond.'
+word_pattern = re.compile('\w+')
+words = word_pattern.findall(text)  # ['If', 'keys', 'values', 'and', 'items', 'views', 'are', 'iterated', 'over', 'with', 'no', 'intervening', 'modifications', 'to', 'the', 'dictionary', 'the', 'order', 'of', 'items', 'will', 'directly', 'correspond']
+
+
+"""生成字符串的简略表示形式（不会超过30个字符）"""
+reprlib.repr(text)  # 'If keys, val...y correspond.'
+
