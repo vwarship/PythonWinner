bd7013cf8157139a4a840f7e31c546b5c2893409
Junjian Wang <vwarship@163.com>
2017-07-25 18:11:25 +0800

    增加urllib.request下载国旗

---
+++ demo/download_flags_requests.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import time
+import urllib.request
+
+
+COUNTRY_CODES = {'CN', 'IN', 'US', 'ID', 'BR', 'PK', 'NG', 'BD', 'RU', 'JP',
+                 'MX', 'PH', 'VN', 'ET', 'EG', 'DE', 'IR', 'TR', 'CD', 'FR'}
+
+BASE_URL = 'http://flupy.org/data/flags/'
+
+DOWNLOAD_DIR = 'downloads/'
+
+
+def print_progress(cc):
+    print(cc, end=' ')
+    sys.stdout.flush()
+
+
+def download_flag(url, filename):
+    response = urllib.request.urlopen(url)
+    img = response.read()
+    path = os.path.join(DOWNLOAD_DIR, filename)
+    with open(path, 'wb') as f:
+        f.write(img)
+
+
+def download_flags(country_codes):
+    for cc in country_codes:
+        cc = cc.lower()
+        url = '{}{cc}/{cc}.gif'.format(BASE_URL, cc=cc)
+        filename = '{}.gif'.format(cc)
+        print_progress(cc)
+        download_flag(url, filename)
+
+
+def main(download_flags):
+    if not os.path.exists(DOWNLOAD_DIR):
+        os.mkdir(DOWNLOAD_DIR)
+
+    begin_time = time.time()
+    download_flags(COUNTRY_CODES)
+    end_time = time.time()
+
+    print('{} flags downloaded in {:.2f}s'.format(len(COUNTRY_CODES), end_time-begin_time))
+
+if __name__ == '__main__':
+    main(download_flags)
