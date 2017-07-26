7f88349d2b1a60cedb4fadc21f0b487e300a20ec
Junjian Wang <vwarship@163.com>
2017-07-26 16:29:01 +0800

    processpool download flags

---
+++ demo/download_flags_processpool.py
@@ -0,0 +1,13 @@
+from concurrent import futures
+
+from demo.download_flags_requests import download_flag_cc, main
+
+
+def download_flags(country_codes):
+    with futures.ProcessPoolExecutor() as executor:
+        res = executor.map(download_flag_cc, country_codes)
+
+    return len(list(res))
+
+if __name__ == '__main__':
+    main(download_flags)
