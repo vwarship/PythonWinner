49ff31ff49f9856a246d819a44cfb92885c1e98d
Junjian Wang <vwarship@163.com>
2017-07-28 01:32:27 +0800

    CPU密集计算，单线程与多线程的比较

--- tutorial/threading_.py
+++ tutorial/threading_.py
@@ -1,4 +1,55 @@
 from threading import Thread, Lock
+from time import time
+
+
+"""
+Python 受GIL的限制，一次只能运行一个线程，所以做CPU密集计算时，多线程的效率反而下降（线程之间切换）。
+run no_thread() used time 0.978
+run multi_thread() used time 1.078
+"""
+def factorize(number):
+    """因式分解算法"""
+    for i in range(1, number+1):
+        if number % i == 0:
+            yield i
+
+
+def used_time(func):
+    start = time()
+    func()
+    end = time()
+    print('run %s() used time %.3f' % (func.__name__, end-start))
+
+
+numbers = [3139079, 2214759, 2516637, 2852285]
+
+
+def no_thread():
+    for number in numbers:
+        list(factorize(number))
+
+
+class FactorizeThread(Thread):
+    def __init__(self, number):
+        super().__init__()
+        self.number = number
+
+    def run(self):
+        self.factors = list(factorize(self.number))
+
+
+def multi_thread():
+    threads = []
+    for number in numbers:
+        thread = FactorizeThread(number)
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+used_time(no_thread)
+used_time(multi_thread)
 
 
 """
