34de859b9e6864660a809400ac4a7ba43a9fadd4
Junjian Wang <vwarship@163.com>
2017-07-27 16:42:46 +0800

    itertools.cycle

---
+++ tutorial/itertools_.py
@@ -0,0 +1,13 @@
+import itertools
+import time
+
+
+"""
+itertools.cycle 一直循环下去
+|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\......
+"""
+# for c in itertools.cycle('|/-\\'):
+#     print(c, end='', flush=True)
+#     time.sleep(0.1)
+
+
