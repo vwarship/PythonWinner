faab2534787e7b401fd169a62db84b0a460078dd
Junjian Wang <vwarship@163.com>
2017-08-04 22:54:34 +0800

    使用列表推荐完成map的功能

--- tutorial/python/map.py
+++ tutorial/python/map.py
@@ -1,3 +1,7 @@
 """map(function, iterable)"""
 ll = list(map(lambda name: 'Hi ' + name + '!', ['Jack', 'Lili']))
 print(ll)   # ['Hi Jack!', 'Hi Lili!']
+
+"""Python3 推荐使用下面的列表推导来完成上面的功能"""
+ll = ['Hi ' + name + '!' for name in ['Jack', 'Lili']]
+print(ll)   # ['Hi Jack!', 'Hi Lili!']
