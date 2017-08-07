8f8eb412292576b2bfb1f2f7d3263a051d99e5e4
Junjian Wang <vwarship@163.com>
2017-08-08 01:34:04 +0800

    画树型图

---
+++ tutorial/matplotlib/__init__.py
@@ -0,0 +1,4 @@
+import matplotlib
+
+
+font_zh = matplotlib.font_manager.FontProperties(fname='/Library/Fonts/Songti.ttc')
--- tutorial/matplotlib/draw_annotate.py
+++ tutorial/matplotlib/draw_annotate.py
@@ -1,12 +1,8 @@
-import matplotlib
 import matplotlib.pyplot as plt
 
+from tutorial.matplotlib import font_zh
 
 # http://matplotlib.org/users/annotations_guide.html
-
-
-# 中文字体
-font_zh = matplotlib.font_manager.FontProperties(fname='/Library/Fonts/Songti.ttc')
 
 
 def draw_annotate(text, xy_begin, xy_text):
