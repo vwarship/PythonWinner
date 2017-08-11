5394ba16685fd11e72800c35d7e46590e4f72ee7
Junjian Wang <vwarship@163.com>
2017-08-12 00:29:12 +0800

    增加了窗口尺寸、标题设置

--- tutorial/mytkinter/hello.py
+++ tutorial/mytkinter/hello.py
@@ -3,6 +3,12 @@ from tkinter import Label
 
 
 tk = Tk()
+
+# 设置窗口的大小宽x高+偏移量。中间的是小写字母x
+tk.geometry('640x480+0+0')
+tk.title('Hello')
+# tk.iconbitmap('logo.ico') # 不起作用
+
 label = Label(tk, text='Hello World!')
 label.grid()
 
