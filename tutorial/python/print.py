d131b7017cae96591ecada0e3de79732a75b4d55
Junjian Wang <vwarship@163.com>
2017-08-14 23:49:46 +0800

    print

--- tutorial/python/print.py
+++ tutorial/python/print.py
@@ -1,3 +1,7 @@
+import sys
+from pprint import pprint
+
+
 peoples = [("wjj", "boy", 36), ("wrj", "girlwj", 8)]
 
 
@@ -14,3 +18,16 @@ wrj        | boy   |   8
 print("{:10} | {:5} | {:5} ".format("name", "sex", "age"))
 for people in peoples:
     print("{:10} | {:5} | {:^5}".format(*people))
+
+
+"""
+['/Users/wjj/GitHub/PythonWinner/tutorial/python',
+ '/Users/wjj/GitHub/PythonWinner',
+ '/Library/Frameworks/Python.framework/Versions/3.6/lib/python36.zip',
+ '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6',
+ '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/lib-dynload',
+ '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages']
+['/Users/wjj/GitHub/PythonWinner/tutorial/python', '/Users/wjj/GitHub/PythonWinner', '/Library/Frameworks/Python.framework/Versions/3.6/lib/python36.zip', '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6', '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/lib-dynload', '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages']
+"""
+pprint(sys.path)
+print(sys.path)
